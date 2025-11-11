9c624ae73ed1a82c535d35fc2b41d9b4060047ec
Sofhia Chromiec <sofhia.chromiec@gmail.com>
2025-11-11 16:55:13 -0300

    declaração dos arquivos onde irao as entidades

---
+++ src/entities/Usuario.java
@@ -0,0 +1,48 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String senha;
+
+    public Usuario() {}
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Getters e Setters
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+               "id=" + id +
+               ", nome='" + nome + '\'' +
+               ", email='" + email + '\'' +
+               '}';
+    }
+}
